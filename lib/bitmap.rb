04b6da3a90e22c627718ce1c6fd9cc9580dd4672
Cristina Iannone <iannonecristina@gmail.com>
2017-07-26 19:25:40 +0100

    TDD color a table's pixel with a specified color

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,16 +1,18 @@
 class Bitmap
-  attr_reader :output_array
+  attr_reader :table
 
   def initialize(parameters)
     @parameters = parameters
-    @output_array = []
+    @table = []
   end
 
-  def paint
-    if @parameters[0] == "I"
-      @output_array << "O" * @parameters[1]
-      @output_array << "O" * @parameters[2] unless @parameters[2] == 1
-    end
+  def create_table
+      @table << "O" * @parameters[1]
+      @table << "O" * @parameters[2] unless @parameters[2] == 1
+  end
+
+  def color_pixels(coordinates_and_colour)
+    @table[coordinates_and_colour[2]-1][coordinates_and_colour[1]-1] = coordinates_and_colour[3]
   end
 
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -3,20 +3,31 @@ require 'bitmap'
 describe Bitmap do
 
   context "One line output" do
-    it "renders a white (O) table at coordinates 1,1" do
+    it "creates a white (O) table at coordinates 1,1" do
       parameters = ["I", 1, 1]
       subject = Bitmap.new(parameters)
-      subject.paint
-      expect(subject.output_array).to eq ["O"]
+      subject.create_table
+      expect(subject.table).to eq ["O"]
     end
   end
 
   context "Two line output" do
-    it "renders a white (O) table at coordinates 2,2" do
+    it "creates a white (O) table at coordinates 2,2" do
       parameters = ["I", 2, 2]
       subject = Bitmap.new(parameters)
-      subject.paint
-      expect(subject.output_array).to eq ["OO","OO"]
+      subject.create_table
+      expect(subject.table).to eq ["OO","OO"]
+    end
+  end
+
+  context "Color a pixel" do
+    it "colors a pixel from the table with a specified color" do
+      parameters = ["I", 2, 2]
+      subject = Bitmap.new(parameters)
+      subject.create_table
+      second_parameters = ["L", 1, 2, "A"]
+      subject.color_pixels(second_parameters)
+      expect(subject.table).to eq ["OO","AO"]
     end
   end
 
